869dba7767f84acf5085848a6a5220fb53896465
Jake Lam <jlam2017@gmail.com>
2024-07-26 14:09:11 -0600

    Adding dynamic unit selection circles

--- Units/Unit.gd
+++ Units/Unit.gd
@@ -19,5 +19,19 @@ func _ready():
 #selecting a unit
 func set_selected(value):
 	selected = value
-	selection.visible = value
+	if isPlayer == true:
+		selection.visible = value
+	elif isPlayer == false:
+		drawEnemyCircle(selection)
+		selection.visible = value
 
+func drawEnemyCircle(selection):
+		var style:StyleBoxFlat = StyleBoxFlat.new()
+		style.border_color =  Color(1,0,0,0.86) #red
+		style.bg_color =  Color(0.86,0,0,0)
+		style.anti_aliasing = false
+		style.set_border_width_all(2)
+		style.corner_detail = 20
+		style.set_corner_radius_all(20)
+		style.border_blend = true
+		selection.add_theme_stylebox_override("panel", style)
