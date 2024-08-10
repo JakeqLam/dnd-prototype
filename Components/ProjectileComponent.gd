69503e23b31e4088defc13937ca0979d2898a70f
Jake Lam <jlam2017@gmail.com>
2024-08-10 15:27:40 -0600

    Adding SFX

--- Components/ProjectileComponent.gd
+++ Components/ProjectileComponent.gd
@@ -3,6 +3,7 @@ extends Area2D
 var dmg:int = 1
 var spd:int = 1
 @export var projectileType: Node2D
+@onready var arrow_impact: AudioStreamPlayer2D = get_node("../arrowImpact")
 
 func _ready():
 	dmg = projectileType.dmg
@@ -17,8 +18,10 @@ func generateDMG(dmgMin, dmgMax):
 func destroy():
 	queue_free()
 func _on_area_entered(_area):
+	arrow_impact.play()
 	destroy()
 func _on_body_entered(_body):
+	arrow_impact.play()
 	destroy()
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
